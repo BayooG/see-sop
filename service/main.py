d80bbd82cd1ab9041cc7b841e4a92a291564d258
bayoog <obaydaba96@gmail.com>
2024-03-12 15:16:09 +0300

    add middleware for CORS

--- service/main.py
+++ service/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
 from service.endpoints.root import router as root_router
 from service.endpoints.mvp import router as mvp_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
 app.include_router(root_router)
 app.include_router(mvp_router)
+
+# Add middleware for CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://mdisinntbif4csncmqi2e56dte0ntupn.lambda-url.eu-central-1.on.aws/"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/", tags=["root"])
