6e88e267651edba17c3e49c40165cb8069fe7a30
bayoog <obaydaba96@gmail.com>
2024-03-15 18:33:43 +0300

    feat: add exception handler

--- service/main.py
+++ service/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+import logging
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+
 from service.endpoints.root import router as root_router
 from service.endpoints.mvp import router as mvp_router
 from fastapi.middleware.cors import CORSMiddleware
@@ -20,6 +24,17 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+# TODO: remove this when in production
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    exc_str = f"{exc}".replace("\n", " ").replace("   ", " ")
+    logging.error(f"{request}: {exc_str}")
+    content = {"status_code": 10422, "message": exc_str, "data": None}
+    return JSONResponse(
+        content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
+    )
 
 
 @app.get("/", tags=["root"])
